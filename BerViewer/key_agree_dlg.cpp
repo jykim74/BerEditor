25fdd87b624084b26aa45b4a112ad143508a933c
jykim74 <jykim74@gmail.com>
2020-05-22 13:50:38 +0900

    Base work

--- BerViewer/key_agree_dlg.cpp
+++ BerViewer/key_agree_dlg.cpp
@@ -14,7 +14,7 @@ const QStringList sECCParamList = {
     "sect131r1", "sect131r2", "sect163k1", "sect163r1", "sect163r2",
     "sect193r1", "sect193r2", "sect233k1", "sect233r1", "sect239k1",
     "sect283k1", "sect283r1", "sect409k1", "sect409r1", "sect571k1",
-    "sect571r1"
+    "sect571r1", "prime256v1"
 };
 
 
