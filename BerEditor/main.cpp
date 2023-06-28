751788714ee5f8b914ebc13cad47d6c54d71b77b
jykim <jykim@ranix.co.kr>
2023-06-28 16:06:11 +0900

    윈도우 기본 폰트 굴림체 지정

--- BerEditor/main.cpp
+++ BerEditor/main.cpp
@@ -23,6 +23,12 @@ int main(int argc, char *argv[])
     qss.open( QFile::ReadOnly );
     app.setStyleSheet(qss.readAll());
 
+#ifdef Q_OS_WIN32
+    QFont font;
+    font.setFamily(QString("굴림체"));
+    app.setFont(font);
+#endif
+
     QCommandLineParser parser;
     parser.setApplicationDescription( QCoreApplication::applicationName());
     parser.addHelpOption();
@@ -43,12 +49,6 @@ int main(int argc, char *argv[])
 
     berApplet->start();
 
-
-#ifdef Q_OS_WIN32
-    QFont font;
-    font.setFamily(QString("굴림체"));
-    app.setFont(font);
-#endif
 
     MainWindow *mw = berApplet->mainWindow();
     if( !parser.positionalArguments().isEmpty() )
