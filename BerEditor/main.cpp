7eb7e21e77c6ca21069d8d60cd4c7109b838d570
jykim <jykim@ranix.co.kr>
2023-08-25 16:23:54 +0900

    OS 별 qss 지원

--- BerEditor/main.cpp
+++ BerEditor/main.cpp
@@ -9,6 +9,30 @@
 #include "crl_info_dlg.h"
 #include "settings_mgr.h"
 
+void setQss( QApplication* app )
+{
+    QString strStyle;
+    QFile qss(":/bereditor.qss");
+    qss.open( QFile::ReadOnly );
+
+    strStyle = qss.readAll();
+
+#if defined( Q_OS_WIN32 )
+    QFile css( ":/qt-win.css" );
+#elif defined( Q_OS_MAC)
+    QFile css( ":/qt-mac.css" );
+#endif
+    css.open( QFile::ReadOnly );
+
+    if( css.size() > 0 )
+    {
+        strStyle += "\n";
+        strStyle += css.readAll();
+    }
+
+    app->setStyleSheet( strStyle );
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -20,9 +44,13 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain( "jssoft.com" );
     QCoreApplication::setApplicationName( "BerEditor" );
 
+#if 0
     QFile qss(":/bereditor.qss");
     qss.open( QFile::ReadOnly );
     app.setStyleSheet(qss.readAll());
+#else
+    setQss( &app );
+#endif
 
     QCommandLineParser parser;
     parser.setApplicationDescription( QCoreApplication::applicationName());
