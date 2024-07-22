9de410d7a4e939a4b4a000ccee793225179081b2
jykim <jykim@ranix.co.kr>
2024-07-22 10:40:04 +0900

    TabOrder 정리

--- BerEditor/data_encoder_dlg.cpp
+++ BerEditor/data_encoder_dlg.cpp
@@ -46,7 +46,7 @@ DataEncoderDlg::DataEncoderDlg(QWidget *parent) :
 #if defined(Q_OS_MAC)
     layout()->setSpacing(5);
 #endif
-    resize(width(), minimumSizeHint().height());
+    resize(minimumSizeHint().width(), minimumSizeHint().height());
 }
 
 DataEncoderDlg::~DataEncoderDlg()
