c3e42d732cd31900de4d5178b9daa3022470e9ae
jykim74 <jykim74@gmail.com>
2024-07-28 11:34:22 +0900

    LCN 텍스트 포커싱

--- BerEditor/lcn_info_dlg.cpp
+++ BerEditor/lcn_info_dlg.cpp
@@ -343,12 +343,14 @@ void LCNInfoDlg::clickGet()
         if( strEmail.length() < 1 )
         {
             berApplet->warningBox( tr("Please enter a email"), this );
+            mEmailText->setFocus();
             return;
         }
 
         if( strKey.length() < 1 )
         {
             berApplet->warningBox( tr("Please enter a license key"), this );
+            mKeyText->setFocus();
             return;
         }
 
