ef74de2430cfb8ff3f5ab8cae709df15e0e7b0ee
jykim <jykim@ranix.co.kr>
2025-01-07 18:46:05 +0900

    AutoUpdate Default 오류 수정

--- BerEditor/settings_dlg.cpp
+++ BerEditor/settings_dlg.cpp
@@ -122,7 +122,12 @@ void SettingsDlg::clickRestoreDefaults()
     bool bVal = berApplet->yesOrNoBox( strMsg, this, false );
     if( bVal == false ) return;
 
-    mgr->removeSet( "Misc", "SparkleAlreadyEnableUpdateByDefault" );
+#ifdef _AUTO_UPDATE
+    if( AutoUpdateService::instance()->shouldSupportAutoUpdate() ) {
+        AutoUpdateService::instance()->setAutoUpdateEnabled(true);
+    }
+#endif
+
     mgr->removeSet( "Language", "current" );
     mgr->removeSet( kBehaviorGroup, kShowPartOnly );
     mgr->removeSet( kBehaviorGroup, kOIDConfigPath );
