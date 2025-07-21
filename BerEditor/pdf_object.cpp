b5f5b320b4c7ef278ee79f258db1a7fe0c6142ad
jykim <jykim@ranix.co.kr>
2025-07-21 11:19:10 +0900

    Base work

--- BerEditor/pdf_object.cpp
+++ BerEditor/pdf_object.cpp
@@ -7,31 +7,18 @@ PDFObject::PDFObject() {}
 
 void PDFObject::test()
 {
+    // X509 Certificate
     /*
-    PdfMemDocument doc(inputPdf.c_str());
-    PdfAcroForm* form = doc.GetAcroForm();
+    string cert;
+    TestUtils::ReadTestInputFile("mycert.der", cert);
 
-    if (!form) {
-        form = doc.CreateAcroForm();
-    }
+    PdfMemDocument doc(stream);
+    auto& page = doc.GetPages().GetPageAt(0);
+    auto& annot = page.GetAnnotations().GetAnnotAt(0);
+    auto& field = dynamic_cast<PdfAnnotationWidget&>(annot).GetField();
+    auto& signature = dynamic_cast<PdfSignature&>(field);
 
-    PdfPage* page = doc.GetPage(0);
-    PdfRect rect(100.0, 100.0, 200.0, 50.0);
-
-    // 서명 필드 생성
-    PdfAnnotation* annot = page->CreateAnnotation(PdfAnnotation::AnnotationType_Widget, rect);
-    PdfField* sigField = form->CreateSignatureField("Signature1", annot);
-    sigField->GetObject()->GetDictionary().AddKey("FT", PdfName("Sig"));
-    sigField->GetObject()->GetDictionary().AddKey("T", PdfString("Signature1"));
-
-    // 빈 /Contents 예약
-    PdfObject* sigObj = new PdfObject();
-    sigObj->GetDictionary().AddKey("Type", PdfName("Sig"));
-    sigObj->GetDictionary().AddKey("Filter", PdfName("Adobe.PPKLite"));
-    sigObj->GetDictionary().AddKey("SubFilter", PdfName("adbe.pkcs7.detached"));
-    sigObj->GetDictionary().AddKey("Contents", PdfString(std::string(8192, 0))); // 서명 공간 확보
-    sigField->GetObject()->GetDictionary().AddKey("V", PdfReference(doc.GetObjects().AddObject(sigObj)));
-
-    doc.Write(outputPdf.c_str());
+    auto signer = PdfSignerCms(cert, pkey);
+    PoDoFo::SignDocument(doc, *stream, signer, signature, PdfSaveOptions::NoMetadataUpdate);
     */
 }
