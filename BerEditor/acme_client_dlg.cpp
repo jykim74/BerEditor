62551e342ca499f696c303940a1c77e3199c08ff
jykim74 <jykim74@gmail.com>
2026-08-02 15:41:55 +0900

    오류 수정

--- BerEditor/acme_client_dlg.cpp
+++ BerEditor/acme_client_dlg.cpp
@@ -728,19 +728,13 @@ int ACMEClientDlg::clickParse()
     if( object["status"].isString() == false )
     {
         nStatus = object["status"].toInt();
-        QString strDetail = object["detail"].toString();
-        berApplet->warningBox( tr("Error: %1 status: %2").arg( strDetail) .arg( nStatus ), this);
-        return -1;
+        if( nStatus >= 300 )
+        {
+            QString strDetail = object["detail"].toString();
+            berApplet->warningBox( tr("Error: %1 status: %2").arg( strDetail) .arg( nStatus ), this);
+            return -1;
+        }
     }
-
-    /*
-    if( nStatus >= 300 )
-    {
-        QString strDetail = object["detail"].toString();
-        berApplet->warningBox( tr("Error: %1 status: %2").arg( strDetail) .arg( nStatus ), this);
-        return -1;
-    }
-    */
 
     if( strCmd.toUpper() == kCmdCertificate.toUpper() )
     {
