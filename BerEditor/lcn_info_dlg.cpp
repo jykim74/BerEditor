885b9575a33e4896522ba6e6e2612641367630b6
jykim74 <jykim74@gmail.com>
2026-08-21 00:16:00 +0900

    LCN Notify 비 라이선스만 요청

--- BerEditor/lcn_info_dlg.cpp
+++ BerEditor/lcn_info_dlg.cpp
@@ -144,13 +144,13 @@ void LCNInfoDlg::initialize()
         mCurGroup->setEnabled( false );
         time_t tLastTime = berApplet->settingsMgr()->getStopMessage();
         if( tLastTime > 0 ) mStopMessageCheck->setChecked(true);
+
+        notifyCheck();
     }
 
     mUpdateBtn->setEnabled( mCurGroup->isEnabled() );
     tabWidget->setCurrentIndex(0);
     mCloseBtn->setDefault(true);
-
-    notifyCheck();
 }
 
 void LCNInfoDlg::settingsLCN( const QString strUser, const BIN *pLCN )
